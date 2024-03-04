b7d43854d92713835bf18505e41bb1a23d794994
Crls_Guerrero <carlos01guerrerope@gmail.com>
2024-03-04 18:43:16 -0500

    add svg from profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,12 @@
 import "../style/Profile.css";
-
+import { ReactComponent as IconDeveloperBackground } from "../svg/developer-background.svg";
 function Profile(){
     return(
         <div className="about">
-
+            <IconDeveloperBackground className="icon-dev"/>
+            <div className="about-me">
+                
+            </div>
         </div>
     );
 }
