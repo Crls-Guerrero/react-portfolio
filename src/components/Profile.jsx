4053724cdfd34edf96f5a8515b541d717589c3f4
Crls_Guerrero <carlos01guerrerope@gmail.com>
2024-03-06 17:41:57 -0500

    profile and CV.pdf

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,26 @@
-import "../style/Profile.css";
 import { ReactComponent as IconDeveloperBackground } from "../svg/developer-background.svg";
+import "../style/Profile.css";
+
+function Curriculum(){
+    window.open("https://drive.google.com/file/d/15CYH7jskF8ho7PIy3cgErwAxgglXq-KX/view?usp=sharing","_blank")
+}
+
 function Profile(){
     return(
         <div className="about">
-            <IconDeveloperBackground className="icon-dev"/>
             <div className="about-me">
-                
+                <h2>Hi!, I'm
+                    <span> C</span>a
+                    <span>r</span>
+                    <span>l</span>o
+                    <span>s</span>
+                </h2>
+                <p>Front-End Developer.</p>
+                <a href="../pdf/CV.pdf" target="_blank">aCV</a>
+                <button onClick={Curriculum}>Curriculum Vitae</button>
             </div>
+            <IconDeveloperBackground className="icon-dev"/>
+            
         </div>
     );
 }
