c897e8c7667f979da1a18b923fe633f014a2715d
Carlos <carlos01guerrerope@gmail.com>
2024-04-08 17:01:55 -0500

    Skills terminado

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Html from "../img/html-5.png";
 import Css from "../img/css-3.png";
 import Boostrap from "../img/boostrap.png"
@@ -14,66 +16,70 @@ import VsCode from "../img/vscode.png"
 import "../style/Skills.css" 
 
 function Skills() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className="skills" id='skills'>
-      <div className="slider">
-        <div class="slide-track">
-            <div class="slide">
+      <div className='slider-track'>
+        <div className="slider">
+            <div class="slide" data-aos="fade-right" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="1000">
               <img src={Html} alt="Html" />
               <p>HTML5</p>
             </div>
-            <div class="slide">
+            <div class="slide" data-aos="fade-right" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="1000">
               <img src={Css} alt="Css" />
-              <p>CSS3</p>
+              <p>CSS3</p> 
             </div>
-            <div class="slide">
+            <div class="slide" data-aos="fade-down" data-aos-offset="250" data-aos-easing="linear" data-aos-duration="1500">
               <img src={Js} alt="Js" />
                 <p>JavaScript</p>
             </div>
-            <div class="slide">
+            <div class="slide" data-aos="fade-left" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="1000">
               <img src={ReactIco} alt="ReactIco" />
                 <p>React</p>
             </div>
-            <div class="slide">
+            <div class="slide" data-aos="fade-left" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="1000">
               <img src={Sql} alt="Sql" />
                 <p>SQL Server</p>
             </div>
-            </div>
-        <div class="slide-track">
-            <div class="slide">
+        </div>
+        <div class="slider">
+            <div class="slide" data-aos="fade-right" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="1500">
               <img src={Github} alt="Github" />
                 <p>Github</p>
             </div>
-            <div class="slide">
+            <div class="slide" data-aos="fade-right" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="1500">
               <img src={Git} alt="Git" />
                 <p>Git</p>
             </div>            
-            <div class="slide">
+            <div class="slide" data-aos="fade-left" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="1500">
               <img src={Boostrap} alt="Bootstrap" />
                 <p>Bootstrap</p>
             </div>
-            <div class="slide">
+            <div class="slide" data-aos="fade-left" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="1500">
               <img src={Tailwind} alt="Tailwind" />
                 <p>Tailwind</p>
             </div>                       
         </div>
-        <div class="slide-track">
-            <div class="slide">
+        <div class="slider">
+            <div class="slide" data-aos="fade-right" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="2000">
               <img src={Php} alt="Php" />
                 <p>Php</p>
             </div>
-            <div class="slide">
+            <div class="slide" data-aos="fade-up" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="2000">
               <img src={Figma} alt="Figma" />
                 <p>Figma</p>
             </div>            
-            <div class="slide">
+            <div class="slide" data-aos="fade-left" data-aos-offset="250" data-aos-easing="ease-in-sine"  data-aos-duration="1500">
               <img src={VsCode} alt="VsCode" />
                 <p>VsCode</p>
-            </div>
-                                
+            </div>                                
         </div>
       </div>
     </div>
+    
   )
 }
 
