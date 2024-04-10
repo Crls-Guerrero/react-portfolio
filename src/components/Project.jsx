7353059b6b305e05a7660d8e0e30f863e564e07d
Carlos <carlos01guerrerope@gmail.com>
2024-04-10 15:23:14 -0500

    Project succes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,37 +1,73 @@
 import React from 'react'
 import "../style/Project.css"
-import Html from "../img/html-5.png";
+import Html from "../img/poued.png"
 
 function Project() {
   return (
     <div className='project'>
-      <div className="all">
-        <h2>All Projects</h2>
-        <h2>FullStack</h2>
-        <h2>Frontend</h2>
-        <h2>Backend</h2>
-      </div>
-      <div className="view-project">
-        <div className='view-card'>
+     <h1>Projects</h1>
 
-          {/* card */}
-          <div className="view-content">
-            <div className='content-to'>
-              <h2>Web Developer</h2>
-              <p>Lorem ipsum dolor sit incidunt vitae aliquam dolorem neque in dolore dicta beatae sit id, possimus eius, unde facilis blanditiis ab.</p>
-              <div className='view-img'>
-                <img src={Html} alt="" />
-              </div>
-            </div>
-            <div className='content-bottom'>
-              <a href="hht">View</a>
-              <a href="htt">Github</a>
-            </div>        
+      {/* Projects */}
+      <div className='card-project'>
+        <div className='img-project'>
+          <img src={Html} alt="Html" />          
+        </div>
+        <div className='content-project'>
+          <div className='description-content'>
+            <p>Desarrollo de un SaaS basado en Inteligencia Artificial que a través de modelos predictivos usa el histórico de datos del usuario y permite mejorar la conversión de leads.</p>
           </div>
-
-          
+          <div className='lenguaje-content'>
+              <div className='lenguaje-part1'>
+                <h3>Tecnologias usadas:</h3>
+                <p>React | TAilwind | AWS</p>
+              </div>
+              <div className='lenguaje-part2'>
+                <a href="hh">Visitar</a>
+              </div>
+          </div>
         </div>
       </div>
+
+      <div className='card-project'>
+        <div className='img-project'>
+          <img src={Html} alt="Html" />          
+        </div>
+        <div className='content-project'>
+          <div className='description-content'>
+            <p>Desarrollo de un SaaS basado en Inteligencia Artificial que a través de modelos predictivos usa el histórico de datos del usuario y permite mejorar la conversión de leads.</p>
+          </div>
+          <div className='lenguaje-content'>
+              <div className='lenguaje-part1'>
+                <h3>Tecnologias usadas:</h3>
+                <p>React | TAilwind | AWS</p>
+              </div>
+              <div className='lenguaje-part2'>
+                <a href="hh">Visitar</a>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='card-project'>
+        <div className='img-project'>
+          <img src={Html} alt="Html" />          
+        </div>
+        <div className='content-project'>
+          <div className='description-content'>
+            <p>Desarrollo de un SaaS basado en Inteligencia Artificial que a través de modelos predictivos usa el histórico de datos del usuario y permite mejorar la conversión de leads.</p>
+          </div>
+          <div className='lenguaje-content'>
+              <div className='lenguaje-part1'>
+                <h3>Tecnologias usadas:</h3>
+                <p>React | TAilwind | AWS</p>
+              </div>
+              <div className='lenguaje-part2'>
+                <a href="hh">Visitar</a>
+              </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
   )
 }
